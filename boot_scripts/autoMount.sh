89dbae0e25827c4aad5d3af568c645df3b9b8606
Daniel Knapp <dknapp@ucsd.edu>
2015-04-04 02:59:19 +0000

    Modified autoMount.sh to create the SDCard directory if it isn't already there before mounting. Modified process_dirs to use sudo in some places it didn't before and get rid of an unnecessary forward slash.

--- boot_scripts/autoMount.sh
+++ boot_scripts/autoMount.sh
@@ -2,4 +2,5 @@
 cardName=$(sudo fdisk -l 2>/dev/null | grep "63.9 GB" | awk '{print substr($2, 0, length($2))}')
 cardPartition=$(sudo fdisk -l 2>/dev/null | grep "$Device Boot" -A 1 | grep "${cardName}" | awk '{print $1}')
 
+sudo mkdir -p /home/ubuntu/AngryBirds/SDCard/
 sudo mount "${cardPartition}" /home/ubuntu/AngryBirds/SDCard/ -t auto
